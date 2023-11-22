9681a9f97be6abf5378545e8238352ab3096fa60
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-22 07:52:56 +0200

    squash!

--- Source/extensionVariables.ts
+++ Source/extensionVariables.ts
@@ -3,7 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AzExtTreeDataProvider, AzExtTreeItem, IAzExtOutputChannel } from "@microsoft/vscode-azext-utils";
+import {
+	AzExtTreeDataProvider,
+	AzExtTreeItem,
+	IAzExtOutputChannel,
+} from "@microsoft/vscode-azext-utils";
 import { AzureHostExtensionApi } from "@microsoft/vscode-azext-utils/hostapi";
 import { ExtensionContext, SecretStorage, TreeView } from "vscode";
 import { DatabasesFileSystem } from "./DatabasesFileSystem";
@@ -19,33 +23,34 @@ import { AzureAccountTreeItemWithAttached } from "./tree/AzureAccountTreeItemWit
  * Namespace for common variables used throughout the extension. They must be initialized in the activate() method of extension.ts
  */
 export namespace ext {
-    export let connectedMongoDB: MongoDatabaseTreeItem | undefined;
-    export let connectedPostgresDB: PostgresDatabaseTreeItem | undefined;
-    export let context: ExtensionContext;
-    export let outputChannel: IAzExtOutputChannel;
-    export let tree: AzExtTreeDataProvider;
-    export let treeView: TreeView<AzExtTreeItem>;
-    export let attachedAccountsNode: AttachedAccountsTreeItem;
-    export let ignoreBundle: boolean | undefined;
-    export let azureAccountTreeItem: AzureAccountTreeItemWithAttached;
-    export let secretStorage: SecretStorage;
-    export let postgresCodeLensProvider: PostgresCodeLensProvider | undefined;
-    export const prefix: string = 'azureDatabases';
-    export let fileSystem: DatabasesFileSystem;
-    export let mongoCodeLensProvider: MongoCodeLensProvider;
-    export let mongoLanguageClient: MongoDBLanguageClient;
-    export let rgApi: AzureHostExtensionApi;
+	export let connectedMongoDB: MongoDatabaseTreeItem | undefined;
+	export let connectedPostgresDB: PostgresDatabaseTreeItem | undefined;
+	export let context: ExtensionContext;
+	export let outputChannel: IAzExtOutputChannel;
+	export let tree: AzExtTreeDataProvider;
+	export let treeView: TreeView<AzExtTreeItem>;
+	export let attachedAccountsNode: AttachedAccountsTreeItem;
+	export let ignoreBundle: boolean | undefined;
+	export let azureAccountTreeItem: AzureAccountTreeItemWithAttached;
+	export let secretStorage: SecretStorage;
+	export let postgresCodeLensProvider: PostgresCodeLensProvider | undefined;
+	export const prefix: string = "azureDatabases";
+	export let fileSystem: DatabasesFileSystem;
+	export let mongoCodeLensProvider: MongoCodeLensProvider;
+	export let mongoLanguageClient: MongoDBLanguageClient;
+	export let rgApi: AzureHostExtensionApi;
 
-    export namespace settingsKeys {
-        export const mongoShellPath = 'mongo.shell.path';
-        export const mongoShellArgs = 'mongo.shell.args';
-        export const documentLabelFields = 'cosmosDB.documentLabelFields';
-        export const enableEndpointDiscovery = 'cosmosDB.enableEndpointDiscovery';
-        export const mongoShellTimeout = 'mongo.shell.timeout';
-        export const batchSize = 'azureDatabases.batchSize';
+	export namespace settingsKeys {
+		export const mongoShellPath = "mongo.shell.path";
+		export const mongoShellArgs = "mongo.shell.args";
+		export const documentLabelFields = "cosmosDB.documentLabelFields";
+		export const enableEndpointDiscovery =
+			"cosmosDB.enableEndpointDiscovery";
+		export const mongoShellTimeout = "mongo.shell.timeout";
+		export const batchSize = "azureDatabases.batchSize";
 
-        export namespace vsCode {
-            export const proxyStrictSSL = "http.proxyStrictSSL";
-        }
-    }
+		export namespace vsCode {
+			export const proxyStrictSSL = "http.proxyStrictSSL";
+		}
+	}
 }
